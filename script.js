61db8d5ee89688798573424df517859d9086e232
Mamata24 <nmamta240@gmail.com>
2020-05-31 21:13:45 +0530

    Marks Recorder

---
+++ script.js
@@ -0,0 +1,82 @@
+var studentDetails = [];
+var nameUI;
+var ageUI;
+var scienceUI;
+var englishUI;
+var mathematicsUI;
+var addStudentUI;
+
+function EnterData() {
+    nameUI = document.querySelector("#name");
+    ageUI = document.querySelector("#age");
+    scienceUI = document.querySelector("#s");
+    englishUI = document.querySelector("#e");
+    mathematicsUI = document.querySelector("#m");
+    addStudentUI = document.querySelector("#submitdata");
+    tableUI = document.querySelector("#showData");
+    showDataUI = document.querySelector("#showDatatable");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    EnterData();
+    addStudentUI.addEventListener("click", createStudent);
+});
+
+function createStudent(e) {
+    e.preventDefault();
+    var obj = {
+        name: nameUI.value,
+        age: ageUI.value,
+        science: scienceUI.value | "0",
+        english: englishUI.value | "0",
+        mathematics: mathematicsUI.value | "0",
+        sum:
+            (parseInt(scienceUI.value) +
+                parseInt(englishUI.value) +
+                parseInt(mathematicsUI.value)
+
+            ) |
+            "0"
+    };
+    studentDetails.push(obj);
+    showData(studentDetails);
+}
+
+function showData(filterData) {
+    var sumnum = 0;
+    showDataUI.innerHTML = "";
+
+    filterData.forEach(function (student) {
+        var trow = document.createElement("tr");
+        sum = 0;
+        for (key in student) {
+            var tbody = document.createElement("td");
+            tbody.className = "tdata";
+            tbody.innerText = student[key];
+            trow.appendChild(tbody);
+        }
+        showDataUI.appendChild(trow);
+    });
+
+    filterData.forEach(function (ele) {
+        sumnum += parseInt(ele.sum);
+    });
+    trow.appendChild(tbody);
+    showDataUI.appendChild(trow);
+}
+var ascendingbtn = document.getElementById("Ascending");
+ascendingbtn.addEventListener("click", ascending);
+function ascending() {
+    var newdata = sum.sort(function (a, b) {
+        return a - b;
+    });
+    showData(newdata);
+}
+var descendingbtn = document.getElementById("Descending");
+descendingbtn.addEventListener("click", descending);
+function descending() {
+    var newdata = sumnum.sort(function (a, b) {
+        return b.average - a.average;
+    });
+    showData(newdata);
+}
